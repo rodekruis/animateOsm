ef2ef3dcb1890f4778276052cc72468f50b99d5a
Raymond Nijssen <r.nijssen@terglobo.nl>
2018-08-20 11:50:25 +0200

    fix for missing python future library

--- plugin/AnimateOsm/networkaccessmanager.py
+++ plugin/AnimateOsm/networkaccessmanager.py
@@ -16,8 +16,8 @@
 *                                                                         *
 ***************************************************************************
 """
-from future import standard_library
-standard_library.install_aliases()
+#from future import standard_library
+#standard_library.install_aliases()
 from builtins import str
 from builtins import object
 
